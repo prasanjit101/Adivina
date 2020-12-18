3d6d3961d8da15cdeb638ec498cef8a346cf362e
prasanjit <57594105+prasanjit101@users.noreply.github.com>
2020-12-19 02:19:19 +0530

    f

--- functions.py
+++ functions.py
@@ -1,9 +1,21 @@
 import random
 import string
+import time
 
 def code():
     n=5
     res=''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase+string.digits,k=n))
     return res
 
-print(code())
+def time_lapse():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time
+
+
+start = time.time()
+while True:
+    end = time.time()
+    if (end - start)>3:
+        print("%.1f" %(end - start))
+        start=end
