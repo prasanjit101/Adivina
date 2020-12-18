216cb799e2bd8057a7657b67d1b490cfb671977f
prasanjit <57594105+prasanjit101@users.noreply.github.com>
2020-12-19 02:22:25 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,15 @@
 import random
 import string
+import time
 
 def code():
     n=5
     res=''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase+string.digits,k=n))
     return res
 
-print(code())
+def time_lapse():
+    start = time.time()
+    while True:
+        end = time.time()
+        if (end - start)>3:
+        #call a function to make the answer time time-out
