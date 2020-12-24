41e60172f22378b230e0426e5b3045e6c46f3e7e
prasanjit101 <prasanjitdutta45@gmail.com>
2020-12-24 20:55:00 +0530

    correccted assign codes

--- templates/roll no.py
+++ templates/roll no.py
@@ -1,9 +1,14 @@
 file = open(r"Roll_number.txt","w")
-print("Enter the static format (like CSB190)")
+print("Enter the static format (like CSB19)")
 string=input()
 print("Enter the starting roll number :")
 s=int(input())
 print("Enter the ending roll number :")
 e=int(input())
 for i in range(s,e+1):
-    file.write(string+str(i)+"\n")
+    if i<10:
+        file.write(string+"00"+str(i)+"\n")
+    elif i<100:
+        file.write(string+"0"+str(i)+"\n")
+    elif i<1000:
+        file.write(string+str(i)+"\n")
