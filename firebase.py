df82075b2e8552c9098aeb7f1804098b24a4bb25
kartikpapney <pdt.kartik@gmail.com>
2020-12-25 23:51:26 +0530

    make it better

--- firebase.py
+++ firebase.py
@@ -22,130 +22,93 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    # x = 5
-    # val = timerFunction(x)
-    # db.child("questions").set(questions)
-    return render_template('home.html', t="Welcome to Adivina")
-
-@app.route('/get', methods=['GET', 'POST'])
-def get_questions():
-    if request.method == 'GET':
-        questions = db.child("questions").get()
-        return render_template('home.html', t=questions.val())
-    return render_template('home.html')
  
 
-@app.route('/admin', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def generate_code():
     if request.method == 'POST':
-        join = code()
-        room = request.form.get('room')
+
+        room = code()
         name = request.form.get('name')
+        admin = request.form.get('admin')
         start = request.form.get('start')
         end = request.form.get('end')
-        val = assign_codes_to_roll_no(int(start), int(end))
-        if test_strings(room, name, val):
-            return render_template('home.html', t="Empty Strings")
-        db.child('rooms').child(join).set({'room': room, 'name': name})
-        db.child('rooms').child(join).child("students").set(val)
-        return render_template('home.html', info="Room Created", code=join, name=name, room=room)
-    return render_template('home.html', info="Error")
 
-@app.route('/admin/join', methods=['POST'])
+        students = assign_codes_to_roll_no(int(start), int(end))
+
+        if test_strings(name, admin, start, end):
+            return render_template('home.html', t="Empty Strings")
+
+        db.child('rooms').child(room).set({'admin': admin, 'name': name})
+        db.child('rooms').child(room).child('students').set(students)
+
+        return render_template('home.html', info="Room Created", room=room, admin=admin, name=name)
+    return render_template('home.html', info="Welcome to Adivina")
+
+
+@app.route('/admin', methods=['POST'])
 def admin_join():
     if request.method == 'POST':
-        join = request.form.get('join')
-        name = request.form.get('name')
-        if test_strings(join, name):
+        room = request.form.get('room')
+        admin = request.form.get('admin')
+        if test_strings(room, admin):
             return render_template('home.html')
         val = db.child('rooms').shallow().get().val()
         for i in val:
-            if i == join:
-                room = db.child('rooms').child(i).get().val()
-                if room['name'] == name:
-                    return render_template('admin.html', room=room)         
+            if i == room:
+                details = db.child('rooms').child(room).get().val()
+                if details['admin'] == admin:
+                    return render_template('admin.html', name=details['name'], room=room)         
         return render_template('home.html', info="Unexpected Error")
 
 
-@app.route('/student/join', methods=['POST'])
+@app.route('/student', methods=['POST'])
 def student_join():
     if request.method == 'POST':
-        join = request.form['join']
-        code = request.form['code']
-        name = request.form['name']
-        if test_strings(join, name):
+        room = request.form.get('room')
+        code = request.form.get('code')
+        name = request.form.get('name')
+        if test_strings(room, name):
             return render_template('home.html', t="Empty Strings")
+
         val = db.child('rooms').shallow().get().val()
         for i in val:
-            if i == join:
-                if db.child('rooms').child(join).child('students').get().val() != None:
-                    student = db.child('rooms').child(join).child('students').get().val()
+            if i == room:
+                if db.child('rooms').child(room).child('students').get().val() != None:
+                    student = db.child('rooms').child(room).child('students').get().val()
                     if len(student) > int(name) and student[int(name)] == code:
-                        return render_template('home.html', t=student[int(name)])
+                        room_name = db.child('rooms').child(room).get().val()['name']
+                        return render_template('student.html', name=room_name, room=room, student=name)
     return render_template('home.html', info="Unexpected Error")
 
-@app.route('/question/add', methods=['POST'])
-def add_question():
+
+
+@app.route('/admin/<room>/add', methods=['POST'])
+def add_question(room):
     if request.method == 'POST':
-        room = request.form['room']
-        admin = request.form['admin']
-        question = request.form['question']
-        time = request.form['time']
-        if test_strings(room, admin, question, time):
-            return render_template('home.html', t="Empty Strings")
-        val = 0
+        question = request.form.get('question')
+        name = request.form.get('name')
+        if test_strings(room, question):
+            return render_template('admin.html', info="Empty Strings")
+        question_number = 0
         if db.child('rooms').child(room).child('questions').shallow().get().val() != None:
-            val = len(db.child('rooms').child(room).child('questions').shallow().get().val())
-        db.child('rooms').child(room).child('questions').child(val).set({'question': question, 'time': time})
-        return render_template('home.html', t="Done")
+            question_number = len(db.child('rooms').child(room).child('questions').shallow().get().val())
+        db.child('rooms').child(room).child('questions').child(question_number).set({'question': question})
+        return render_template('admin.html', info="Uploaded", room=room, name=name)
 
 
-
-@app.route('/question/get', methods=['POST'])
-def get_question():
+@app.route('/<room>/<student>/answer', methods=['POST'])
+def ans_question(room, student):
     if request.method == 'POST':
-        room = request.form['room']
-        if test_strings(room):
-            return render_template('home.html', t="Empty Strings")
-        val = 0
-        if db.child('rooms').child(room).child('questions').shallow().get().val() != None:
-            val = len(db.child('rooms').child(room).child('questions').shallow().get().val())
-        if(val == 0):
-            return render_template('home.html', t="No Question")
-        que = "No Question"
-        
-        return render_template('home.html', t=db.child('rooms').child(room).child('questions').child(val - 1).get().val())
-
-
-@app.route('/question/answer', methods=['POST'])
-def ans_question():
-    if request.method == 'POST':
-        room = request.form['room']
-        name = request.form['name']
-        index = request.form['index']
         answer = request.form['answer']
-        if test_strings(room, name, index, answer):
-            return render_template('home.html', t="Empty Strings")
-
-        db.child('rooms').child(room).child('questions').child(index).child('answers').child(name).set({'answer': answer})
-        return render_template('home.html', t=answer)
-
-
-@app.route('/rooms/get', methods=['GET'])
-def get_rooms():
-    if request.method == 'GET':
-        val = db.child('rooms').get().val()
-        return render_template('home.html', t=val)
-    return render_template('home.html')
-
-
-@app.route('/start', methods=['POST'])
-def start_viva():
-    if request.method == 'POST':
-        return render_template('home.html', t="Entered")
+        if test_strings(answer):
+            return render_template('student.html', t="Empty Strings")
+        if db.child('rooms').child(room).child('questions').shallow().get().val() != None:
+            question_number = len(db.child('rooms').child(room).child('questions').shallow().get().val())
+            question = db.child('rooms').child(room).child('questions').child(question_number - 1).get().val()['question']
+            db.child('rooms').child(room).child('questions').child(question_number - 1).child('answers').child(student).set({'answer': answer})
+            return render_template('student.html', info="Uploaded", room=room, student=student, question=question)
+        return render_template('student.html', info="Unexpected Error")
         
 
 if __name__ == '__main__': 
